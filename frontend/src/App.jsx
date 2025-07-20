5a8305ac1936833250a2a407f59e31ec498982c8
Nikhil <paidynikhil@gmail.com>
2025-07-20 14:43:41 +0530

    react tailwind setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Toaster position="top-right" />
+      <Routes>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
